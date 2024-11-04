6b23ab702894e44ca85deaff91c4d196c9dd63f1
Cleber Fadil <cleber.fadil@gmail.com>
2024-11-04 12:47:10 -0300

    Post New Category

--- src/main/java/dev/galileu/aulaspring/controller/CategoryController.java
+++ src/main/java/dev/galileu/aulaspring/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package dev.galileu.aulaspring.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import dev.galileu.aulaspring.dto.CategoryDTO;
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/categories")
@@ -29,4 +33,12 @@ public class CategoryController {
         CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);    
     }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+        
 }
