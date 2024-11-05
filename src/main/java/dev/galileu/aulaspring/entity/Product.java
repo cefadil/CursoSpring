d0db1dbda42549454aba7de3178bd7c3b5fcfab1
Cleber Fadil <cleber.fadil@gmail.com>
2024-11-04 21:15:04 -0300

    Product Entity, SQL Script

---
+++ src/main/java/dev/galileu/aulaspring/entity/Product.java
@@ -0,0 +1,40 @@
+package dev.galileu.aulaspring.entity;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tb_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), 
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    Set<Category> categories = new HashSet<>();
+
+}
